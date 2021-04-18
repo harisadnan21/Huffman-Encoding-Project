d34ecc36b411cfe071d69e59cecbbf2872d97ea7
Robertson Waweru <robertson.waweru@duke.edu>
2021-04-18 19:12:34 -0400

    Updated Commit

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -1,3 +1,4 @@
+import java.util.PriorityQueue;
 
 /**
  * Although this class has a history of several years,
@@ -43,11 +44,13 @@ public class HuffProcessor {
 	public void compress(BitInputStream in, BitOutputStream out){
 
 		// remove all this code when implementing compress
-		while (true){
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1) break;
-			out.writeBits(BITS_PER_WORD, val);
-		}
+		int[] counts = readForCounts(in);
+		HuffNode root = makeTreeFromCounts(counts);
+		String[] codings = makeCodingsFromTree(root);
+		out.writeBits(BITS_PER_INT, HUFF_TREE);
+		writeHeader(root, out);
+		in.reset();
+		writeCompressedBits(codings, in, out);
 		out.close();
 	}
 	/**
@@ -65,14 +68,116 @@ public class HuffProcessor {
 		if (magic != HUFF_TREE) {
 			throw new HuffException("invalid magic number "+magic);
 		}
-		// remove all code below this point for P7
-
-		out.writeBits(BITS_PER_INT,magic);
+		HuffNode root = readTree(in);
+		HuffNode current = root;
+		// out.writeBits(BITS_PER_INT,magic);
 		while (true){
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1) break;
-			out.writeBits(BITS_PER_WORD, val);
+			int bits = in.readBits(BITS_PER_WORD);
+			if (bits == -1) {
+				throw new HuffException("bad input, no PSEUDO_EOF");
+			}
+			else{
+				if (bits == 0) {current = current.myLeft;}
+				else {current = current.myRight;}
+
+				if (current.myLeft== null && current.myRight==null){
+					if (current.myValue == PSEUDO_EOF){break;}
+					else{
+						out.writeBits(BITS_PER_WORD, current.myValue);
+						current = root;
+					}
+				}
+			}
 		}
 		out.close();
+	}
+
+	private HuffNode readTree(BitInputStream in){
+		int bit = in.readBits(1);
+		if (bit == -1){
+			throw new HuffException("failed reading bits");
+		}
+		if (bit == 0){
+			HuffNode left = readTree(in);
+			HuffNode right = readTree(in);
+			return new HuffNode(0, 0, left, right);
+		}
+		else{
+			int value = in.readBits(BITS_PER_WORD+1);
+			return new HuffNode(value,0, null, null);
+		}
+	}
+
+	private int[]  readForCounts(BitInputStream in){
+		int[] arr = new int[ALPH_SIZE + 1];
+		while(true){
+			int val = in.readBits(BITS_PER_WORD);
+			arr[val] += 1;
+			if(val==-1) {
+				break;
+			}
+		}
+		arr[PSEUDO_EOF] = 1;
+		return arr;
+	}
+
+	private HuffNode makeTreeFromCounts(int[] counts) {
+		PriorityQueue<HuffNode> pq = new PriorityQueue<>();
+		for (int i = 0; i < counts.length; i++) {
+			if (counts[i] > 0) {
+				pq.add(new HuffNode(i, counts[i], null, null));
+			}
+		}
+		while (pq.size() > 1) {
+			HuffNode left = pq.remove();
+			HuffNode right = pq.remove();
+			HuffNode t = new HuffNode(0, left.myWeight + right.myWeight, left, right);
+			pq.add(t);
+		}
+		HuffNode root = pq.remove();
+		return root;
+	}
+
+	private String[] makeCodingsFromTree(HuffNode root){
+		String[] encodings = new String[ALPH_SIZE + 1];
+		codingHelper(root, "", encodings);
+		return encodings;
+	}
+
+	private void codingHelper(HuffNode root,String path, String[] encodings) {
+		if (root == null) return;
+		if (root.myLeft == null && root.myRight == null) {
+			encodings[root.myValue] = path;
+			return;
+		} else {
+			codingHelper(root.myLeft, path + "0", encodings);
+			codingHelper(root.myRight, path + "1", encodings);
+		}
+	}
+
+	private void writeHeader(HuffNode root, BitOutputStream out){
+		if (root.myLeft != null && root.myRight != null){
+			out.writeBits(1, 0);
+			writeHeader(root.myLeft, out);
+			writeHeader(root.myRight, out);
+			//return new HuffNode(0, 0, left, right);
+		}
+		else if (root.myLeft == null && root.myRight == null){
+			out.writeBits(1, 1);
+			out.writeBits(BITS_PER_WORD+1, root.myValue);
+		}
+	}
+
+	private void writeCompressedBits(String[] codings, BitInputStream in, BitOutputStream out){
+		while(true) {
+			int val = in.readBits(BITS_PER_WORD);
+			if (val == -1) {
+				break;
+			}
+			String code = codings[val];
+			out.writeBits(code.length(), Integer.parseInt(code, 2));
+		}
+		String code = codings[PSEUDO_EOF];
+		out.writeBits(code.length(), Integer.parseInt(code, 2));
 	}
 }
